51b6adbb6133a18ebf5eb6905e2e7c3ae2a5d036
mccallumj-crypto <mccallumj@apexcs.net>
2026-08-09 10:44:36 -0400

    Delete Public directory

--- Public/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='civilbid-shell-v1';
-const ASSETS=['/','/manifest.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));});
