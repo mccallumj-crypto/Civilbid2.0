27ffb8430038b6a8ef2bd6d40c86dc630b462d3a
mccallumj-crypto <mccallumj@apexcs.net>
2026-09-24 18:38:34 -0400

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -9,7 +9,13 @@ export async function updateSession(request: NextRequest) {
     {
       cookies: {
         getAll() { return request.cookies.getAll() },
-        setAll(cookiesToSet) {
+        setAll(
+  cookiesToSet: {
+    name: string
+    value: string
+    options?: Record<string, any>
+  }[]
+) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           response = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options))
